c06154b092cf9025ef3dd28fbf14889031377747
javaclass <javaclass@15000502CPU.fec.org>
2017-09-27 15:23:58 -0500

    Loop & Reverse Loop

---
+++ Intro/src/basics1/Loop1.java
@@ -0,0 +1,19 @@
+package basics1;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		
+
+	int i=1;{
+	
+		while(i<=10) {
+			System.out.println("Kameron");
+			
+			i=i+ 1;
+		}
+
+}
+}
+}
+
---
+++ Intro/src/basics1/Loop2.java
@@ -0,0 +1,18 @@
+package basics1;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		int n=10;{
+			
+			while(n>=1) {
+				System.out.println(n);
+				
+				n=n-1;
+
+	}
+
+}
+}
+}
+
